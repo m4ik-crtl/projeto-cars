f16df9681ec5db1ea1894720b3ca54b43821dadd
Maikon Silva <55884605+m4ik-crtl@users.noreply.github.com>
2025-05-10 13:11:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('vehicles_us.csv')
+st.header("Análise de Anúncios de Carros")
+
+if st.checkbox("Mostrar histograma da quilometragem"):
+    st.write("Histograma da coluna odometer")
+    fig = px.histogram(df, x="odometer")
+    st.plotly_chart(fig, use_container_width=True)
+
+if st.checkbox("Mostrar gráfico de dispersão preço x ano"):
+    st.write("Gráfico de dispersão entre preço e ano do modelo")
+    fig = px.scatter(df, x="model_year", y="price", color="type")
+    st.plotly_chart(fig, use_container_width=True)
 
